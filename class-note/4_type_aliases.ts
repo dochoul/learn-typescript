6c16f0c24946569444a07df5f41039d010805510
dochoul <dochoul@gmail.com>
2022-05-15 18:17:17 +0900

    type aliase

---
+++ class-note/4_type_aliases.ts
@@ -0,0 +1,14 @@
+type typedPerson = {
+  name: string,
+  age: number
+}
+
+interface interfacePerson {
+  name: string,
+  age: number
+}
+
+let soso: typedPerson = {
+  name: 'soso',
+  age: 66
+}
